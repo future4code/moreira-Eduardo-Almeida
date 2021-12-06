5edf69b4c77c8ed0dbb127393c8641aa6deafebb
Eduardo Almeida 20 <prog.eduardoalmeida@gmail.com>
2021-12-06 16:30:17 -0300

    exercicios objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,67 @@
+// EXERCÍCIO DE LEITURA DE CÓDIGOS_____________________________________________________________________
+// 1) Leia o código abaixo
+
+// const filme = {
+// 	nome: "Auto da Compadecida", 
+// 	ano: 2000, 
+// 	elenco: [
+// 		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+// 		"Virginia Cavendish"
+// 		], 
+// 	transmissoesHoje: [
+// 		{canal: "Telecine", horario: "21h"}, 
+// 		{canal: "Canal Brasil", horario: "19h"}, 
+// 		{canal: "Globo", horario: "14h"}
+// 		]
+// }
+
+// console.log(filme.elenco[0])
+// console.log(filme.elenco[filme.elenco.length - 1])
+// console.log(filme.transmissoesHoje[2])
+
+// a) O primeiro console.log irá imprimir o nome Matheus Nachtergaele, o segundo irá informar o nome
+// Virginia Cavendish. O último console.log aprensetará Globo e 14hrs.]
+
+// 2) Leia o código abaixo: 
+
+// const cachorro = {
+// 	nome: "Juca", 
+// 	idade: 3, 
+// 	raca: "SRD"
+// }
+
+// const gato = {...cachorro, nome: "Juba"}
+
+// const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+// console.log(cachorro)
+// console.log(gato)
+// console.log(tartaruga)
+
+// a) Irá imprimir as mesmas informações para idade e raça nos três consoles.log, no entanto o cachorro
+// se chamará Juca, o gato será Juba e a tartaruga Jubo.
+
+// b) adiciona um elemento à lista.
+
+// 3) Leia o código abaixo.
+
+// function minhaFuncao(objeto, propriedade) {
+// 	return objeto[propriedade]
+// }
+
+// const pessoa = {
+//   nome: "Caio", 
+//   idade: 23, 
+//   backender: false
+// }
+
+// console.log(minhaFuncao(pessoa, "backender"))
+// console.log(minhaFuncao(pessoa, "altura"))
+
+// a) O primerio console irá imprimir a informação que está nas chaves da const pessoa, no caso
+// informar de que é false de que ela se trate de um backender. O segundo console será undefined.
+
+// b) O segundo console foi undefined, pois não há nenhuma variável atribuída como "altura" 
+
+// EXERCÍCIOS DE ESCRITURA DE CÓDIGO______________________________________________________________________
+
