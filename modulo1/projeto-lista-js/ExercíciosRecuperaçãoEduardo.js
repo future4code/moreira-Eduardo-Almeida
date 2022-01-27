9abf91aa00b06ae75f48ba56077bbb4c19be845e
Eduardo Almeida 20 <prog.eduardoalmeida@gmail.com>
2022-01-27 17:01:23 -0300

    Exercícios de Recuperação Eduardo Almeida 28-01-22

---
+++ modulo1/projeto-lista-js/ExercíciosRecuperaçãoEduardo.js
@@ -0,0 +1,52 @@
+// EXERCÍCIO 01
+
+// const altura = Number(prompt("Digite a altura"))
+// const largura = Number(prompt("Digite a largura"))
+// const area = [altura,largura]
+
+// function calculaAreaRetangulo(calculoArea) {
+//   const resultadoArea = calculoArea[0] * calculoArea [1]
+//   return resultadoArea
+// }
+// console.log(calculaAreaRetangulo(area))
+
+// EXERCÍCIO 03
+// const peso = Number(prompt("Digite o seu peso"))
+// const altura = Number(prompt("Digite a sua altura"))
+// const dadosInseridos = [peso, altura]
+
+// function calculaIMC(pesoEAltura) {
+//     const imc = pesoEAltura[0] / (pesoEAltura[1] * pesoEAltura[1])
+//    return imc.toFixed(2)  
+//   }
+//  console.log(calculaIMC(dadosInseridos))
+
+// EXERCÍCIO 05
+//  const cor1 = prompt("Digite uma cor")
+//  const cor2 = prompt("Digite outra cor")   
+//  const cor3 = prompt("Digite mais uma cor") 
+//  const coresFavoritas = [cor1, cor2, cor3]
+
+//   function imprimeTresCoresFavoritas(lista) {
+//        return lista
+//   }
+//  console.log(imprimeTresCoresFavoritas(coresFavoritas))
+
+// EXERCÍCIO 07
+// function calculaIngressosEspetaculo(custo, valorIngresso) {
+// const quantidadeIngressos = custo / valorIngresso
+// return quantidadeIngressos
+  
+//   }
+// console.log(calculaIngressosEspetaculo(8000, 50))
+
+// EXERCÍCIO 11
+
+// const lista = [1,3,5,7,11]
+// console.log(lista)
+// function trocaPrimeiroEUltimo(array) {
+//     array.reverse()
+//    return array
+  
+//   }
+// console.log(trocaPrimeiroEUltimo(lista))
